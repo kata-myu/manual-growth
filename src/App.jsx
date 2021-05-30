dc6c154d5ad98b6ff98ec1df738c0d649910803c
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-05-30 16:18:40 +0900

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./components/header/header";
+
+const App = () => {
+  return(
+    <div>
+      <Header></Header>
+    </div>
+  );
+};
+export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return(
+    <div className="header">
+      <span className="header_title">ManualGrowth</span>
+    </div>
+  );
+};
+export default Header;
